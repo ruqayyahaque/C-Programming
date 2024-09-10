d7243642792339a40a60edfad7f6587c1dce9600
ruqayyahaque <ruqayyahaque20@gmail.com>
2024-09-10 13:51:42 +0600

    Add files via upload

---
+++ hhh.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int num1,num2,input;
+ printf("enter two numbers:");
+ scanf("%d %d", &num1, &num2);
+ input= num1*num2/2;
+
+ printf("Input=%d", input);
+ return 0;
+
+
+}
